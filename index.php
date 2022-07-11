a7abd8e2d5fe77a561fc6aae7cc0774639f3028b
kike <ccvianney@gmail.com>
2022-07-11 11:24:25 -0600

    login

--- index.php
+++ index.php
@@ -5,9 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app/css/bootstrap.css">
+    <link rel="stylesheet" href="app/css/style.css">
     <title>Bienvenidos a HEYCLINIC!</title>
 </head>
-<body>
-    <h1>Hola mundo</h1>
+<body class="m-0 vh-100 row justify-content-center align-items-center">
+    <div class="col-md-6 ingresar">
+        <div class="row">
+            <div class="col-6 col-xs-12">
+              <img src="img/login.jpg" alt="Imagen login" style="width:100%">
+            </div>
+
+            <div class="col-6 col-xs-12 p-4">
+              <div class="col-12"><h1 class="text-center">HEYLAB!</h1></div>
+
+              <div class="col-12 mt-4">
+                <input type="text" class="form-control" placeholder="Usuario" autofocus>
+              </div>
+              <div class="col-12 mt-3">
+                <input type="password" class="form-control" placeholder="Clave">
+              </div>
+              <div class="col-12 mt-3 d-grid"><button class="btn btn-primary btn-block">Ingresar</button></div>
+
+              <div class="col mt-4 version"><p class="text-center"><b>Version 1.0</b></p></div>
+            </div>
+
+
+        </div>
+    </div>
 </body>
+
+<script src="app/css/boostrap.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </html>
