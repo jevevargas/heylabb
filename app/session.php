3781597e1fd496012d064a9b22b3b8b28a377f7e
kike <ccvianney@gmail.com>
2022-07-12 09:51:19 -0600

    session

---
+++ app/session.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+
+if(!isset($_SESSION['idusuario'])){
+   
+    $url='../index.php';
+    header("Location: $url");
+    }else{
+    $idusuario = $_SESSION['idusuario'];
+    $nombre = $_SESSION['nombre'];
+    $idtipousuario = $_SESSION['idtipousuario'];  //echo $tipo;
+    $idcomercio = $_SESSION['idcomercio'];
+    $nombrecompleto = $_SESSION['nombrecompleto'];
+    //$modfecha = $_SESSION['fechamod'];
+   // $nombrecompleto = $_SESSION['nombrecompleto'];
+    }
